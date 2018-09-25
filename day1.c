98b36a7bb1bb5513db895fe58f69ba521344a5b1
sanathkumar0 <43368296+sanathkumar0@users.noreply.github.com>
2018-09-25 12:34:16 +0530

    day1.c
    programs of day1 assignment

---
+++ day1.c
@@ -0,0 +1,17 @@
+\\first program
+#include<stdio.h>
+void main()
+{
+printf("\n hello world ");
+}
+
+\\second program
+#include<stdio.h>
+void main()
+{
+  char a[20];
+  printf("enter your name\n");
+  gets(a);
+  printf("\n your name is :");
+  puts(a);
+}
